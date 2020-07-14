c28348d67cdf03e514300cb344a506ab062ed679
Sendhuraan NKK <>
2020-07-15 02:20:37 +0530

    hello world for cpp

---
+++ 01_hello-world/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Sendhuraan on 7/15/20.
+//  Copyright © 2020 Sendhuraan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Complete Modern C++
+[Video]
+By Umar Lone
+May 2020
+Learn the new C++ 11, 14, and 17 features with the most comprehensive C++ tutorial
